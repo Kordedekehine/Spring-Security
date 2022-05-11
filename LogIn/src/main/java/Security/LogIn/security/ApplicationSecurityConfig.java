b21abbd0395537b2b65b00b4373e20cfb4e7e403
amigoscode <salamikehinde345@gmail.com>
2022-05-11 12:41:37 +0100

    password encoder

--- LogIn/src/main/java/Security/LogIn/security/ApplicationSecurityConfig.java
+++ LogIn/src/main/java/Security/LogIn/security/ApplicationSecurityConfig.java
@@ -1,14 +1,29 @@
 package Security.LogIn.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //we extend here basically to do the basic authentication
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -20,5 +35,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        //user details help us retrieve user from the databases
+        //the user Details is an interface
+              UserDetails jibolaPasuma = User.builder()
+                .username("Jibola Pasuma")
+                .password(passwordEncoder.encode("password"))
+                .roles("student")
+                      .build();
+
+              return new InMemoryUserDetailsManager(jibolaPasuma);
     }
 }
